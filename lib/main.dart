c0924182efee181f10dbb53dd59316f88e7d48db
SalmaAlbakr <salalbakr19988@gmail.com>
2023-10-20 03:15:33 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
-
-
-import 'package:dream_catcher_app/about_page.dart';
+import 'package:dream_catcher_app/screen/register_screen.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
 
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('ar'),
+        startLocale: Locale('ar'),
+        child: MyApp(),),
+  );
+
 }
 class MyApp extends StatelessWidget {
   MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-        home: AboutPage()
-      // _user.currentUser == null ? StartScreen() : HomeScreen()
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.fallbackLocale,
+        home: RegisterScreen()
     );
   }
 }
---
+++ lib/screen/register_screen.dart
@@ -0,0 +1,294 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  // final _auth = FirebaseAuth.instance;
+ // bool _saving = false;
+  late String email;
+  late String password;
+   bool isObscure = true;
+
+  // bool internet = true;
+  // @override
+  // void initState() {
+  //   getCurrentUser();
+  //   super.initState();
+  //   Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+  //     if (result == ConnectivityResult.none) {
+  //       setState(() {
+  //         internet = false;
+  //       });
+  //     } else {
+  //       setState(() {
+  //         internet = true;
+  //       });
+  //     }
+  //   });
+  // }
+
+  GlobalKey<FormState> formKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    // if (internet == false) {
+    //   return NoInternetScreen();
+    // }
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      "AppName".tr(),
+                      // style: GoogleFonts.alexandria(
+                      //   color: Colors.blue[800],
+                      //   fontSize: 40,
+                      // ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+
+                    // first name
+                    Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextFormField(
+                          validator: (String? value) {
+                            if (value == "" || value!.contains("@ / .com")) {
+                              return " من فضلك أدخل الحساب و يجب ان يحتوي علي @ / com. ";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            email = value;
+                          },
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "FirstName".tr(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // last name
+                    Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextFormField(
+                          validator: (String? value) {
+                            if (value == "" || value!.contains("@ / .com")) {
+                              return " من فضلك أدخل الحساب و يجب ان يحتوي علي @ / com. ";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            email = value;
+                          },
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "LastName".tr(),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // phone no.
+                    Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextFormField(
+                          validator: (String? value) {
+                            if (value == "" || value!.contains("@ / .com")) {
+                              return " من فضلك أدخل الحساب و يجب ان يحتوي علي @ / com. ";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            email = value;
+                          },
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "PhoneNO".tr(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+
+                    // password
+                    Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                validator: (String? value) {
+                                  if (value == "" ||
+                                      value!.contains("0123456789") ||
+                                      value.length < 6) {
+                                    return "  كلمة المرور يجب ان تحتوي علي رقم ولا تقل عن 6 حروف ";
+                                  }
+                                  return null;
+                                },
+                                keyboardType: TextInputType.visiblePassword,
+                                obscureText: isObscure,
+                                onChanged: (value) {
+                                  password = value;
+                                },
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password".tr(),
+                                ),
+                              ),
+                            ),
+                            isObscure
+                                ? IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isObscure = false;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.visibility_off_outlined,
+                                      color: Colors.blue,
+                                    ),
+                                  )
+                                : IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isObscure = true;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.remove_red_eye,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+
+                    // Expanded(
+                    //   child: SizedBox(),
+                    // ),
+                    Container(
+                      width: 300,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                           Colors.cyan ,
+                          ),
+                        ),
+                        onPressed: () async {
+                          // if (formKey.currentState!.validate()) {
+                          //   await validation_done(context);
+                          // } else {
+                          //   ScaffoldMessenger.of(context).showSnackBar(
+                          //     SnackBar(
+                          //       backgroundColor: Colors.grey[600],
+                          //       content: Text("يجب ملء البيانات"),
+                          //     ),
+                          //   );
+                          // }
+                        },
+                        child: Text(
+                          "NewAccount".tr(),
+                          style: TextStyle(
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Navigator.of(context).push(
+                        //   MaterialPageRoute(
+                        //     builder: (context) => SignInScreen(),
+                        //   ),
+                        // );
+                      },
+                      child: Text("HaveAccount".tr()),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+      ),
+    );
+  }
+
+  // Future<void> validation_done(BuildContext context) async {
+  //   final isExistingUser = await checkUserExists(email);
+  //   if (isExistingUser) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(
+  //         backgroundColor: Colors.blueAccent,
+  //         content: Text("الحساب موجود بالفعل"),
+  //       ),
+  //     );
+  //   } else {
+  //     await add_new_user(context);
+  //   }
+  // }
+
+  // Future<void> add_new_user(BuildContext context) async {
+  //   setState(() {
+  //     _saving = true;
+  //   });
+  //   try {
+  //     await _auth.createUserWithEmailAndPassword(
+  //         email: email, password: password);
+  //     Navigator.of(context).pushReplacement(
+  //       MaterialPageRoute(
+  //         builder: (context) => HomeScreen(),
+  //       ),
+  //     );
+  //     setState(() {
+  //       _saving = false;
+  //     });
+  //   } catch (e) {
+  //     print("the problem $e");
+  //   }
+  // }
+}

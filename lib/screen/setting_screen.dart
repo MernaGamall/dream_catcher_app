28975bc0d69e18aaa402f231ef6b60b0f4893b81
SalmaAlbakr <salalbakr19988@gmail.com>
2023-10-31 00:40:10 +0200

    first commit

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatefulWidget {
@@ -10,16 +11,18 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-    title: Row(
-    children: [
-    Icon(Icons.settings),
-    SizedBox(width: 5,),
-    Text("setting".tr()),
-    ],
-    ),
-    ),
+        title: Row(
+          children: [
+            Icon(Icons.settings),
+            SizedBox(
+              width: 5,
+            ),
+            Text("setting".tr()),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
@@ -28,44 +31,50 @@ class _SettingScreenState extends State<SettingScreen> {
                 width: MediaQuery.of(context).size.width,
                 child: ElevatedButton(
                     style: ButtonStyle(
-                      // backgroundColor: MaterialStateProperty.all(Colors.white),
-                    ),
+                        // backgroundColor: MaterialStateProperty.all(Colors.white),
+                        ),
                     clipBehavior: Clip.hardEdge,
-                    onPressed: (){
+                    onPressed: () {
                       //   Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ChangeColor()));
-
-                    }, child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.g_translate_outlined),
-                    SizedBox(width: 5,),
-                    Text("Lang".tr(),style: TextStyle(color: Colors.white),),
-                  ],
-                )),
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.g_translate_outlined),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Lang".tr(),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    )),
               ),
               Container(
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                    onPressed: () => FirstScreen.of(context).changeTheme(ThemeMode.light),
-                child: Icon(Icons.add_a_photo_outlined) ,
-                )
-                ),
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    onPressed: () =>
+                        FirstScreen.of(context).changeTheme(ThemeMode.light),
+                    child: Icon(Icons.add_a_photo_outlined),
+                  )),
               Container(
                 width: MediaQuery.of(context).size.width,
                 child: ElevatedButton(
-                    onPressed: () => FirstScreen.of(context).changeTheme(ThemeMode.dark),
+                    onPressed: () =>
+                        FirstScreen.of(context).changeTheme(ThemeMode.dark),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Icon(Icons.dark_mode_outlined),
-                        SizedBox(width: 5,),
+                        SizedBox(
+                          width: 5,
+                        ),
                         Text("Dark".tr()),
                       ],
                     )),
-
               ),
             ],
-
           ),
         ),
       ),

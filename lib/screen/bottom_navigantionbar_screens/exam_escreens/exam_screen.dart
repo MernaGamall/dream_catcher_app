31509bb61eb5ca6024c5720a49f28d8bece9edfa
SalmaAlbakr <salalbakr19988@gmail.com>
2023-11-17 01:35:52 +0200

    first commit

--- lib/screen/bottom_navigantionbar_screens/exam_escreens/exam_screen.dart
+++ lib/screen/bottom_navigantionbar_screens/exam_escreens/exam_screen.dart
@@ -12,6 +12,50 @@ class _ExamScreenState extends State<ExamScreen> {
   Color buttonColor = Colors.grey;
   bool? theAnswer;
 
+  List examQu = [
+    {
+      "title": "any quition we can use1",
+      "answer1": "1",
+      "answer2": "2",
+      "answer3": "3",
+      "answer4": "4",
+      "trueAnswre" : "answer2"
+    },
+    {
+      "title": "any quition we can use2",
+      "answer1": "5",
+      "answer2": "6",
+      "answer3": "7",
+      "answer4": "8",
+      "trueAnswre" : "answer1"
+    },
+    {
+      "title": "any quition we can use3",
+      "answer1": "19",
+      "answer2": "21",
+      "answer3": "352",
+      "answer4": "45",
+
+    "trueAnswre" : "answer4"
+    },
+    {
+      "title": "any quition we can use4",
+      "answer1": "154",
+      "answer2": "256",
+      "answer3": "38",
+      "answer4": "48",
+    "trueAnswre" : "answer1"
+    },
+    {
+      "title": "any quition we can use5",
+      "answer1": "15",
+      "answer2": "2585",
+      "answer3": "385",
+      "answer4": "485",
+      "trueAnswre" : "answer3"
+    }
+  ];
+
   void answer (){
     setState(() {
       buttonColor = Colors.green;
@@ -31,18 +75,27 @@ if (theAnswer == true){
         title: Text(TotalScore.toString()),
       ),
       body: ListView.builder(
-          itemCount: 20,
+          itemCount: examQu.length,
           itemBuilder: (BuildContext context, int item){
         return Container(
           child: Column(
             children: [
-              Text("السؤال رقم $item"),
+              Text(examQu[item]["title"]),
 
               ElevatedButton(
                 onPressed: (){
                   answer();
               },
-                child: Text("answers") ,
+                child: Text(examQu[item]["answer1"]) ,
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(buttonColor)
+              ),),
+              ElevatedButton(
+                onPressed: (){
+                  answer();
+
+              },
+                child: Text(examQu[item]["answer2"]) ,
                 style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(buttonColor)
               ),),
@@ -50,7 +103,7 @@ if (theAnswer == true){
                 onPressed: (){
                   answer();
               },
-                child: Text("answers") ,
+                child: Text(examQu[item]["answer3"]) ,
                 style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(buttonColor)
               ),),
@@ -58,15 +111,7 @@ if (theAnswer == true){
                 onPressed: (){
                   answer();
               },
-                child: Text("answers") ,
-                style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(buttonColor)
-              ),),
-              ElevatedButton(
-                onPressed: (){
-                  answer();
-              },
-                child: Text("answers") ,
+                child: Text(examQu[item]["answer4"]) ,
                 style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(buttonColor)
               ),),
--- lib/todo_file.dart
+++ lib/todo_file.dart
@@ -1,5 +1,45 @@
-/// TODO : edit screen (like register screen)
-///
-///TODO :add apps from mindsit (calc - note - toss ..) in drawer
-///
-///
+// /// TODO : edit screen (like register screen)
+// ///
+// ///TODO :add apps from mindsit (calc - note - toss ..) in drawer
+// class Exam {
+//   List examQu = [
+//  {
+//       "title": "any quition we can use",
+//       "answer1": "1",
+//       "answer2": "2",
+//       "answer3": "3",
+//       "answer4": "4",
+//     },
+//  {
+//       "title": "any quition we can use",
+//       "answer1": "1",
+//       "answer2": "2",
+//       "answer3": "3",
+//       "answer4": "4",
+//     },
+//  {
+//       "title": "any quition we can use",
+//       "answer1": "1",
+//       "answer2": "2",
+//       "answer3": "3",
+//       "answer4": "4",
+//     },
+//  {
+//       "title": "any quition we can use",
+//       "answer1": "1",
+//       "answer2": "2",
+//       "answer3": "3",
+//       "answer4": "4",
+//     },
+//  {
+//       "title": "any quition we can use",
+//       "answer1": "1",
+//       "answer2": "2",
+//       "answer3": "3",
+//       "answer4": "4",
+//     }
+//   ];
+// List examList = examQu.values.toList();
+// //  examQu.entries.forEach((e) => examList.add(Customer(e.key, e.value)));
+//
+// }

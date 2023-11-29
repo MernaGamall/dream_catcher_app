c26d9f0c2fe4b0c6983c3a74f29bef5a9c3c9f99
SalmaAlbakr <salalbakr19988@gmail.com>
2023-11-30 00:48:30 +0200

    first commit

--- lib/screen/bottom_navigantionbar_screens/exam_and_exercies_screens/choose_one.dart
+++ lib/screen/bottom_navigantionbar_screens/exam_and_exercies_screens/choose_one.dart
@@ -1,6 +1,4 @@
 import 'package:dream_catcher_app/screen/bottom_navigantionbar_screens/exam_and_exercies_screens/choose_subject.dart';
-import 'package:dream_catcher_app/screen/bottom_navigantionbar_screens/exam_and_exercies_screens/exam_list.dart';
-import 'package:dream_catcher_app/screen/bottom_navigantionbar_screens/video_screens/videos_screens.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -29,9 +27,10 @@ class _ChooseOneScreenState extends State<ChooseOneScreen> {
               Text(
                 "timeFor".tr(),
                 style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                    color: Colors.red),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.red,
+                ),
               ),
               SizedBox(
                 height: 20,
@@ -52,62 +51,67 @@ class _ChooseOneScreenState extends State<ChooseOneScreen> {
                         );
                       },
                       child: Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xffFFE8C7),
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                  height: 150,
-                                  width: 150,
-                                  child: Icon(
-                                    Icons.pending_actions_rounded,
-                                    size: 80,
-                                  )),
-                              Text(
-                                "Exams".tr(),
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
+                        decoration: BoxDecoration(
+                          color: Color(0xffFFE8C7),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 150,
+                              width: 150,
+                              child: Icon(
+                                Icons.pending_actions_rounded,
+                                size: 80,
                               ),
-                            ],
-                          )),
+                            ),
+                            Text(
+                              "Exams".tr(),
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-
-                       Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => ChooseSubjectScreen(
-                                    yourChoose: "Exercises".tr(),
-                                  ),
-                                ),
-                              );
-                            },
-                            child: Container(
-                                decoration: BoxDecoration(
-                                  color: Color(0xffFFE8C7),
-                                ),
-                                child: Column(
-                                  children: [
-                                    Container(
-                                      height: 150,
-                                      width: 150,
-                                      child: Icon(
-                                        Icons.receipt_long_rounded,
-                                        size: 80,
-                                      ),
-                                    ),
-                                    Text("Exercises".tr(),
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold)),
-                                  ],
-                                )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => ChooseSubjectScreen(
+                              yourChoose: "Exercises".tr(),
+                            ),
                           ),
-                        )
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffFFE8C7),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 150,
+                              width: 150,
+                              child: Icon(
+                                Icons.receipt_long_rounded,
+                                size: 80,
+                              ),
+                            ),
+                            Text(
+                              "Exercises".tr(),
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               )
             ],

89f0dc440eac90cf30df51d3669ea4ba46edc7a1
SalmaAlbakr <salalbakr19988@gmail.com>
2023-11-29 23:40:02 +0200

    first commit

--- lib/screen/authentication_screens/login_screen.dart
+++ lib/screen/authentication_screens/login_screen.dart
@@ -13,94 +13,93 @@ class _LoginScreenState extends State<LoginScreen> {
   GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xffE7DFD5),
       body: SafeArea(
-          child: Center(
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  Container(
-                    width: 350,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blue),
+        child: Center(
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Container(
+                  width: 350,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "PhoneNO".tr(),
+                      ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "PhoneNO".tr(),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: 350,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            validator: (String? value) {
+                              if (value == "") {
+                                return "falsePassword".tr();
+                              }
+                              return null;
+                            },
+                            obscureText: isObscure,
+                            keyboardType: TextInputType.visiblePassword,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password".tr(),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Container(
-                    width: 350,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blue),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              validator: (String? value) {
-                                if (value == "") {
-                                  return "ادخل كلمة المرور الصحيحه";
-                                }
-                                return null;
-                              },
-                              obscureText: isObscure,
-                              keyboardType: TextInputType.visiblePassword,
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Password".tr(),
+                        isObscure
+                            ? IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isObscure = false;
+                                  });
+                                },
+                                icon: Icon(
+                                  Icons.visibility_off_outlined,
+                                  color: Colors.blue,
+                                ),
+                              )
+                            : IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isObscure = true;
+                                  });
+                                },
+                                icon: Icon(
+                                  Icons.remove_red_eye,
+                                  color: Colors.blue,
+                                ),
                               ),
-                            ),
-                          ),
-                           isObscure
-                               ?
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                isObscure = false;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.visibility_off_outlined,
-                              color: Colors.blue,
-                            ),
-                          )
-                              : IconButton(
-                            onPressed: () {
-                              setState(() {
-                                isObscure = true;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.remove_red_eye,
-                              color: Colors.blue,
-                            ),
-                          ),
-                        ],
-                      ),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    child: SizedBox(),
-                  ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: SizedBox(),
+                ),
+              ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
-
